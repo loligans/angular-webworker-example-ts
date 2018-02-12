c8c48fadc2f8ff44adc15f9509bfd67a3a2001a7
loligans <lindemann.ethan@gmail.com>
2018-02-11 20:17:19 -0800

    Switch build system to gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var spawn = require('child_process').spawnSync;
+var yargs = require('yargs').argv;
+
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var clean = require('gulp-clean');
+var nodemon = require('gulp-nodemon');
+
+var serverTsProject = ts.createProject('app/tsconfig.json');
+
+gulp.task('clean', () => {
+  return gulp.src('dist/**/*', {read: false}).pipe(clean());
+});
+
+gulp.task('build', ['clean'], () => {
+  return serverTsProject.src()
+                        .pipe(sourcemaps.init())
+                        .pipe(serverTsProject())
+                        .js
+                        .pipe(babel())
+                        //.pipe(uglify())
+                        .pipe(sourcemaps.write('.'))
+                        .pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('start', ['build'], () => {
+  return spawn('node', ['dist/app/main.js'], {stdio: 'inherit'});
+});
