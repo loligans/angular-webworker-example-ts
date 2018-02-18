b4cb71efc8ea3ba1d24c9ea78b4b42e6038cd810
loligans <lindemann.ethan@gmail.com>
2018-02-17 22:39:44 -0800

    Angular App is mostly up and running.

--- genetic-algorithm-client/app/app.component.ts
+++ genetic-algorithm-client/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
 import { Candidate } from '../../genetic-algorithm/candidate';
 import { GeneticAlgorithm } from '../../genetic-algorithm/genetic-algorithm';
 
@@ -9,5 +8,40 @@ import { GeneticAlgorithm } from '../../genetic-algorithm/genetic-algorithm';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private GA: GeneticAlgorithm;
+  private GAWorker: Worker;
+  public Columns: Array<string> = ['Rank', 'Fitness', 'Chromosome']
+  public Population: Array<Candidate> = [];
+  public Solution: string = "How are you gentleman? All your base are belong to us... How are you gentleman? All your base are belong to us... How are you gentleman? All your base are belong to us...";
+  public PopulationSize: number = 500;
+  public Generations: number = 1000;
+
+  public getString(chars: Array<number>): string {
+    return String.fromCharCode(...chars);
+  }
+
+  public startGeneticAlgorithm(): void {
+    this.GA = new GeneticAlgorithm(500, this.Solution);
+    this.Population = this.GA.Population.slice(0, 10);
+  }
+
+  public runGeneticAlgorithm(ga: GeneticAlgorithm) {
+    console.log('Inside Web Worker')
+    new Promise((resolve) => {
+      ga.computeGeneration();
+      console.log('Computed generation');
+      // for (var i = 0; i < this.Generations; i++) {
+      //   var solution = this.GA.computeGeneration();
   
+      //   if (i % 2000 == 0) {
+      //     this.Population = this.GA.Population.slice(0, 10);
+      //   }
+      //   if (solution) {
+      //     break;
+      //   }
+      // }
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 }
--- genetic-algorithm-client/app/app.module.ts
+++ genetic-algorithm-client/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { CdkTableModule } from '@angular/cdk/table';
-import { MatTableModule } from '@angular/material/table';
-
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatListModule } from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -13,9 +19,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CdkTableModule,
-    MatTableModule
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatListModule,
+    MatSliderModule,
+    MatCardModule
   ],
+  entryComponents: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
