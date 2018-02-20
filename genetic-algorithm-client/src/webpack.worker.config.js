d6b54f7c58a731eb31c70f582b5bb71a793c30b9
loligans <lindemann.ethan@gmail.com>
2018-02-20 12:45:02 -0800

    installed webpack and worker-loader. Prepared the webpack worker config

---
+++ genetic-algorithm-client/src/webpack.worker.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const ProgressPlugin = require('webpack/lib/ProgressPlugin');
+const { NoEmitOnErrorsPlugin } = require('webpack');
+const { AotPlugin } = require('@ngtools/webpack');
+
+module.exports = {
+  "devtool": "source-map",
+  "resolve": {
+    "extensions": [
+      ".ts",
+      ".js"
+    ],
+    "modules": [
+      "./node_modules",
+      "./node_modules"
+    ]
+  },
+  "entry": {
+    "../out/genetic-algorithm.worker.bundle": [
+      './app/workers/genetic-algorithm.worker.ts'
+    ]
+  },
+
+  "output": {
+    "path": process.cwd(),
+    "filename": "[name].js"
+  },
+
+  "watch": false,
+
+  "module": {
+    "rules": [
+      {
+        "enforce": "pre",
+        "test": /\.js$/,
+        "loader": "source-map-loader",
+        "exclude": [
+          /\/node_modules\//
+        ]
+      },
+      {
+        "test": /\.json$/,
+        "loader": "json-loader"
+      },
+      {
+        "test": /\.ts$/,
+        "loader": "@ngtools/webpack"
+      }
+    ]
+  },
+  "plugins": [
+    new NoEmitOnErrorsPlugin(),
+    new ProgressPlugin(),
+    new AotPlugin({
+      "mainPath": "main.ts",
+      "hostReplacementPaths": {
+        "environments/environment.ts": "environments/environment.ts"
+      },
+      "exclude": [],
+      "tsConfigPath": "tsconfig.json",
+      "skipCodeGeneration": true
+    })
+  ],
+  "node": {
+    "fs": "empty",
+    "global": true,
+    "crypto": "empty",
+    "tls": "empty",
+    "net": "empty",
+    "process": true,
+    "module": false,
+    "clearImmediate": false,
+    "setImmediate": false
+  }
+};
